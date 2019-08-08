479b1e07e356736bfe8574492886f98dcad50331
Gitea <gitea@fake.local>
2019-08-08 11:47:56 +0800

    first commit

---
+++ Bassinet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bassinet
+//
+//  Created by i_jhuhuchen on 2019/8/7.
+//  Copyright © 2019 i_jhuhuchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
